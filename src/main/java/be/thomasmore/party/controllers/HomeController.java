80bd5817b0bbbb84f218373209abe47e5151b59c
plato21 <noahjockmans@gmail.com>
2023-02-14 11:41:44 +0100

    alles tot pay.html zie bundel

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,16 +1,23 @@
 package be.thomasmore.party.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping({"/", "/home"})
-    public String home(){
+    public String home(Model model){
         return "home";
     }
     @GetMapping("/about")
-    public String about(){
+    public String about(Model model){
+        String myName = "Noah Jockmans";
+        String myCity = "Muizen";
+        String myStreet = "Laurierlaan 2";
+        model.addAttribute("myCity", myCity);
+        model.addAttribute("myName", myName);
+        model.addAttribute("myStreet", myStreet);
         return "about";
     }
 
